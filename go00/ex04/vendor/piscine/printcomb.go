521f07658e7dd8e00d7a38b956fe5576ca4cbd6c
Ishi <syi378039@gmail.com>
2024-06-11 12:43:24 +0900

    add : go00 ex04

---
+++ go00/ex04/vendor/piscine/printcomb.go
@@ -0,0 +1,19 @@
+package piscine
+
+import "ft"
+
+func PrintComb() {
+  for i := '0'; i <= '7'; i++ {
+    for j := i + 1; j <= '8'; j++ {
+      for k := j + 1; k <= '9'; k++ {
+        ft.PrintRune(i)
+        ft.PrintRune(j)
+        ft.PrintRune(k)
+        if i != '7' {
+          ft.PrintRune(',')
+          ft.PrintRune(' ')
+        }
+      }
+    }
+  }
+}
